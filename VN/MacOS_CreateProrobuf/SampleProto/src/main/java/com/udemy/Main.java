14146f9ba36c6663fd88cf392ea33f805588c4ca
supercoder.udemy <supercoder.udemy@gmail.com>
2021-03-21 13:15:48 +0700

    changes: change the code map with video content

--- VN/MacOS_CreateProrobuf/SampleProto/src/main/java/com/udemy/Main.java
+++ VN/MacOS_CreateProrobuf/SampleProto/src/main/java/com/udemy/Main.java
@@ -1,16 +1,9 @@
 package com.udemy;
 
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-
 import java.io.IOException;
 
 public class Main {
-    public static void main(String [] args) throws IOException, InterruptedException {
-        Server server = ServerBuilder.forPort(8080).addService(new BookServiceImpl())
-                .build();
-        server.start();
-        System.out.println("Book grpc server started");
-        server.awaitTermination();
-    }
+  public static void main(String[] args) throws IOException, InterruptedException {
+    System.out.println("Main started");
+  }
 }
